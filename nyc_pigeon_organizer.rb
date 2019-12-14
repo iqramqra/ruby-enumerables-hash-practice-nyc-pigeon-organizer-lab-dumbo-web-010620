2901c616561980925cca93f8a3251911857dc83c
Iqra Masroor <masrooriqra@gmail.com>
2019-12-14 01:56:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,3 +6,19 @@ def nyc_pigeon_organizer(data)
       all_names.each do |name|
         if pigeon_list
 end
+
+def nyc_pigeon_organizer(data)
+  pigeon_list = {}
+
+  data.each do |attribute_name, attributes|
+    attributes.each do |attribute_value, pigeon_names|
+      pigeon_names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][attribute_name] ||= []
+        pigeon_list[name][attribute_name].push(attribute_value.to_s)
+      end
+    end 
+  end
+
+  pigeon_list
+end
