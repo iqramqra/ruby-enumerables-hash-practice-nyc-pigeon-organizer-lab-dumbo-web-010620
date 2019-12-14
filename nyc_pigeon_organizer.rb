5ec8785f02c6be95e15f121682fe3b178b93ab9e
Iqra Masroor <masrooriqra@gmail.com>
2019-12-14 02:00:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,9 @@
 def nyc_pigeon_organizer(data)
 final = {}
-
   data.each do |first_level, all_other|
     all_other.each do |category, array|
       array.each do |name|
-        final[name] = {:color => [], :gender => [], :lives => []}
+        final[name] = {:color =>[], :gender =>[], :lives => []}
       end 
     end 
   end 
